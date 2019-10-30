3393118f3f295457e1107049182b5762ebcebab8
Your Name <you@example.com>
2019-10-30 09:59:20 -0300

    Alterado

---
+++ controllers/postagens.js
@@ -0,0 +1,29 @@
+const PostagemModel = require('../models/postagem');
+
+class Postagens { 
+
+  async get () { 
+    const postagens = await PostagemModel.find({});
+    return postagens;
+  }
+  
+  //async getPorId (id)
+  async create (postagemDTO){
+    await new PostagemModel(postagemDTO).save();
+  }
+
+  async getPorId(id) {
+    const postagem = await PostagemModel.find({_id: id});
+    return postagem;
+  }
+
+  async update (id, postagemDTO){
+    await PostagemModel.updateOne({_id: id}, postagemDTO);
+  }
+
+  async delete (id, postagemDTO){
+    await PostagemModel.deleteOne({_id: id}, postagemDTO);
+  }
+}
+
+module.exports = new Postagens();
